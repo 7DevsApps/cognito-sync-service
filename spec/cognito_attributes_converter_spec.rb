9274d6bf2ad42a5f998af397dfd8c453443bcecc
Mark Osipenko <mark.osipenko@gmail.com>
2019-07-19 12:49:52 +0300

    remove extra spec

--- spec/cognito_attributes_converter_spec.rb
+++ spec/cognito_attributes_converter_spec.rb
@@ -8,25 +8,40 @@ RSpec.describe CognitoAttributesConverter do
     include ::CognitoAttributesConverter
 
     def cognito_custom_attr_keys
-      [:disabled_at, "restored_at"]
+      [:disabled_at, "restored_at", "has_picture"]
     end
   end
 
   describe '.convert_to_cognito' do
-    let(:attrs1) { { email: "sample@gmail.com", phone_number: "+3123123123", disabled_at: "2020-10-10", restored_at: "2020-10-10" } }
-    let(:attrs2) { { "email" => "sample@gmail.com", "phone_number" => "+3123123123", "disabled_at" => "2020-10-10", "restored_at" => "2020-10-10" } }
+    let(:attrs1) { {
+      email: "sample@gmail.com",
+      phone_number: "+3123123123",
+      disabled_at: "2020-10-10",
+      restored_at: "2020-10-10",
+      has_picture: true,
+      not_white_list_attr: 1 } }
+
+    let(:attrs2) { {
+      "email" => "sample@gmail.com",
+      "phone_number" => "+3123123123",
+      "disabled_at" => "2020-10-10",
+      "restored_at" => "2020-10-10",
+      "has_picture" => true,
+      "not_white_list_attr" => 1
+    } }
 
     let(:user) { UserExample.new }
     let(:cognito_attrs1) do
       [
-        { "name" => "email",        "value" => "sample@gmail.com" },
-        { "name" => "phone_number", "value" => "+3123123123" },
-        { "name" => "custom:disabled_at",  "value" => "2020-10-10" },
-        { "name" => "custom:restored_at",  "value" => "2020-10-10" }
+        { name: "email",              value: "sample@gmail.com" },
+        { name: "phone_number",       value: "+3123123123" },
+        { name: "custom:disabled_at", value: "2020-10-10" },
+        { name: "custom:restored_at", value: "2020-10-10" },
+        { name: "custom:has_picture", value: true }
       ]
     end
 
-    context do
+    context 'white list attributes' do
       it { expect(user.convert_to_cognito(attrs1)).to eq(cognito_attrs1) }
       it { expect(user.convert_to_cognito(attrs2)).to eq(cognito_attrs1) }
     end
--- spec/cognito_sync_service_spec.rb
+++ spec/cognito_sync_service_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe CognitoSyncService do
   it 'has a version number' do
     expect(CognitoSyncService::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
