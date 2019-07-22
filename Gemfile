cd5c982d0f0caa0681954cfbc5137de61cb47be4
Mark Osipenko <mark.osipenko@gmail.com>
2019-07-22 15:56:28 +0300

    added common cognito provider for all ruby projects

--- Gemfile
+++ Gemfile
@@ -3,13 +3,20 @@
 source 'https://rubygems.org'
 
 group :development, :test do
+  # aws
+  gem 'aws-sdk-cognitoidentity', '~> 1.0.0.rc7'
+  gem 'aws-sdk-cognitoidentityprovider', '~> 1.12'
+  gem 'aws-sdk-cognitosync', '~> 1.6'
+  gem 'aws-sdk-ec2', '~> 1'
+  gem 'aws-sdk-s3', '~> 1'
+  gem 'aws-sdk-ses', '~> 1.6'
+
   gem 'pry', '~> 0.10', require: false
   gem 'pry-byebug', '~> 3.4'
   gem 'pry-doc', '~> 1.0.0', require: false
-  gem 'pry-rails', '~> 0.3'
 
   gem 'factory_bot', '~> 4.8'
-  gem 'rspec-rails', '~> 3.6'
+  gem 'rspec', '~> 3.6'
   gem 'rubocop', require: false
   gem 'rubocop-rspec', require: false
 end
--- lib/cognito_provider.rb
+++ lib/cognito_provider.rb
@@ -1,17 +1,30 @@
 # frozen_string_literal: true
 
-# TODO: add to credentials
-# aws:
-#  access_key_id: "access_key_id"
-#  secret_access_key: "secret_access_key"
-#  region: "region"
-#
+# TODO: add to credentials on ENV
+# development:
+#   aws:
+#   access_key_id: "access_key_id"
+#   secret_access_key: "secret_access_key"
+#   region: "region"
+
+require 'aws-sdk-cognitoidentityprovider'
+
 module CognitoProvider
-  def cognito_provider
-    ::Aws::CognitoIdentityProvider::Client.new(
-      access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-      secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-      region: Rails.application.credentials.dig(:aws, Rails.env.to_sym, :region)
+  def cognito_provider(env_key = 'development')
+  ::Aws::CognitoIdentityProvider::Client.new(
+      access_key_id: ENV[env_key]['aws']['access_key_id'],
+      secret_access_key: ENV[env_key]['aws']['secret_access_key'],
+      region: ENV[env_key]['aws']['region']
     )
   end
+  # If you store you env variables in credentials file for rails you should redefine cognito_provider like example bellow
+
+  # def cognito_provider
+  #   ::Aws::CognitoIdentityProvider::Client.new(
+  #     access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+  #     secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+  #     region: Rails.application.credentials.dig(:aws, Rails.env.to_sym, :region)
+  #   )
+  # end
 end
+
