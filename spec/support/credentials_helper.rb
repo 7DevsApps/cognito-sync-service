54a92792ed7aff7c09b2bd676cf01908ab7e4c04
Andrew Alyabyev <nodeleaks@gmail.com>
2019-07-26 14:50:22 +0300

    add comment to credentials storing

--- spec/support/credentials_helper.rb
+++ spec/support/credentials_helper.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
+
+# We assume that you will store your aws credentials in credentials.enc.yml or have similar file like credentials.yml
 require 'yaml'
 CREDENTIALS = YAML.load_file('spec/support/credentials.yml')
 
 module CredentialsHelper
   def aws_provider
     ::Aws::CognitoIdentityProvider::Client.new(
-        access_key_id: CREDENTIALS['development']['aws']['access_key_id'],
-        secret_access_key: CREDENTIALS['development']['aws']['secret_access_key'],
-        region: CREDENTIALS['development']['aws']['region']
+      access_key_id: CREDENTIALS['development']['aws']['access_key_id'],
+      secret_access_key: CREDENTIALS['development']['aws']['secret_access_key'],
+      region: CREDENTIALS['development']['aws']['region']
     )
   end
 
