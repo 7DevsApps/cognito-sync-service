ea6e398c169470ff227a8c9499994b0887ccb128
Andrew Alyabyev <nodeleaks@gmail.com>
2019-07-30 16:30:03 +0300

    add ca_initiate_auth! and spec for that

--- lib/cognito_sync_service.rb
+++ lib/cognito_sync_service.rb
@@ -13,9 +13,9 @@ module CognitoSyncService
   # username - can be email, phone_number or custom string depend on you cognito pool settings
   # attrs - hash of user attributes which will be saved in cognito pool
   # attrs = { email: 'qwe@qwe,com', phone_number:  '+12......0'}
-  def ca_create!(attrs, username)
+  def ca_create!(attrs, username, temporary_password=nil)
     c_attributes = convert_to_cognito(attrs)
-    user = cognito_provider.admin_create_user(user_pool_id: web_pool_id, username: username, user_attributes: c_attributes).user
+    user = cognito_provider.admin_create_user(user_pool_id: web_pool_id, username: username, user_attributes: c_attributes, temporary_password: temporary_password).user
     convert_from_cognito(user)
   end
 
@@ -30,7 +30,7 @@ module CognitoSyncService
     convert_from_cognito(user)
   end
 
-  # username - can be email, phone_number or custom string depend on you cognito pool settings
+  # username - can be email, phone_number or custom string depend on cognito pool settings
   # attrs - hash of user attributes which will be saved in cognito pool
   # attrs = { email: 'qwe@qwe,com', phone_number:  '+12......0'}
   def ca_update!(attrs, username)
@@ -42,5 +42,18 @@ module CognitoSyncService
   # user can be disable by email or phone_number depend on cognito pool settings
   def ca_disable!(username)
     cognito_provider.admin_disable_user(user_pool_id: web_pool_id, username: username)
+  end
+
+  # user can be authenticated by email or phone_number depend on cognito pool settings
+  def ca_initiate_auth!(username, password)
+    cognito_provider.admin_initiate_auth(
+      user_pool_id: web_pool_id,
+      client_id: web_client_id,
+      auth_flow: 'ADMIN_NO_SRP_AUTH',
+      auth_parameters: {
+        USERNAME: username,
+        PASSWORD: password
+      }
+    )
   end
 end
