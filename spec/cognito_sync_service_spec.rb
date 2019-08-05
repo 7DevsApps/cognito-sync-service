00c5e4e389ca7269ed3a9a6fcfbe835ceedc2e47
Andrew Alyabyev <nodeleaks@gmail.com>
2019-08-05 13:42:20 +0300

    add context with email as username and temporary password to #ca_create!

--- spec/cognito_sync_service_spec.rb
+++ spec/cognito_sync_service_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe CognitoSyncService do
   end
 
   describe '#ca_create!' do
-    context 'with valid phone_number in username' do
+    context 'with valid phone_number as username' do
       let!(:phone_number) { "+3333333333" }
       let!(:attrs) { { phone_number: phone_number } }
       let(:user) { UserExample.ca_create!(attrs, phone_number) }
@@ -29,7 +29,15 @@ RSpec.describe CognitoSyncService do
       after { UserExample.ca_delete!(phone_number) }
     end
 
-    xcontext 'with temporary password' do
+    context 'with email as username and temporary password' do
+      let!(:email) { "qwe@qwe.com" }
+      let!(:temporary_password) { 'Qazwsx-edc1!' }
+      let!(:attrs) { { email: email } }
+      let!(:user) { UserExample.ca_create!(attrs, email, temporary_password) }
+
+      it { expect(UserExample.ca_initiate_auth!(email, temporary_password).challenge_name).to eq('NEW_PASSWORD_REQUIRED') }
+
+      after { UserExample.ca_delete!(email) }
     end
   end
 
